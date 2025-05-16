aa7e76ad3fc0124a0fcabac81be6859f68bc02e8
NIKHIL479 <nikhilkacham15@gmail.com>
2025-05-16 19:03:12 -0400

    updated user page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Form from './Form';
 
 function App() {
-  return <div><h1>OhioHealth User Form</h1><Form /></div>;
+  return <div><h1>OhioHealth User Form Page</h1><Form /></div>;
 }
 
 export default App;
